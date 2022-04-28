aaeb8a21bac415e7fd6c9beea9f63d061936fded
John Arian Malondras <arian.malondras@gpi.com.ph>
2022-04-28 15:58:53 +0800

    Logbook Sales - Modal

--- logbookSales.php
+++ logbookSales.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="./styles/styles.css?v=<?php echo time(); ?>">
     <link rel="icon" href="./obj/canteen.png">
     <title>Logbook Sales</title>
+
+    <script src="./sweetalert.min.js"></script>
 </head>
 
 <body id="logbookBody" onload="navFuntion()">
@@ -18,25 +20,80 @@
 
     <div class="lbkCon">
         <div class="lbkToolBar">
-            <input type="text" class="lbkSearch">
+            <input type="text" class="lbkSearch" placeholder="Search..." >
             <button class="lbkAdd" onclick="lbkAdd()">Add</button>
         </div>
 
-        <div class="lbkTableCon">
-            <table class="lbkTable">
-                <thead>
+        <div class="contentCon">
+            <div class="lbkTableCon">
+                <table class="lbkTable">
+                    <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Name</th>
+                            <th>Employer</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                        <tr>
+                            <td>2022/04/28</td>
+                            <td>Test Name</td>
+                            <td>Test Employer</td>
+                            <td class="lgbkAction">
+                                <a href="#" class="lgbkEdit">Edit</a>
+                                <a href="#" class="lgbkDelete">Delete</a>
+                            </td>
+                        </tr>
+
+                        <tr>
+                            <td>2022/04/28</td>
+                            <td>Test Name2</td>
+                            <td>Test Employer2</td>
+                            <td class="lgbkAction">
+                                <a href="#" class="lgbkEdit">Edit</a>
+                                <a href="#" class="lgbkDelete">Delete</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="lgbkBG">
+        <div class="lgbkModal">
+            <form action="POST" class="lgbkForm">
+                <h1 class="lgbkFormTitle">ADD</h1>
+                <table class="lgbkFormTable">
                     <tr>
-                        <th>Date</th>
-                        <th>Name</th>
-                        <th>Employer</th>
-                        <th>Action</th>
+                        <td>Date</td>
+                        <td><input type="date" name="" id=""></td>
                     </tr>
-                </thead>
-
-                <tbody>
-
-                </tbody>
-            </table>
+                    <tr>
+                        <td>Full Name</td>
+                        <td><input type="text" name="" id=""></td>
+                    </tr>
+                    <tr>
+                        <td>Employer</td>
+                        <td>
+                            <select id="empOption" name="empOption" change onchange="FkeyDown()">
+                            <option value="" disabled hidden selected>Select your option</option>
+                            <option value="glory">GLORY</option>
+                            <option value="maxim">MAXIM</option>
+                            <option value="nippi">NIPPI</option>
+                            <option value="powerlane">POWERLANE</option>
+                            <option value="service provider">SERVICE PROVIDER</option>
+                        </td>
+                    </tr>
+                </table>
+                <div class="lgbkModalBtn">
+                    <input type="button" name="lgbkSaveBtn" class="lgbkSaveBtn" id="lgbkSaveBtn" value="Save" onclick="lgbkSaveBtn()">
+                    <input type="button" name="lgbkCloseModal" class="lgbkCloseModal" id="lgbkCloseModal" value="Cancel" onclick="lgbkCloseModal()">
+                </div>
+            </form>
         </div>
     </div>
     
@@ -47,9 +104,12 @@
 
             var wRep = document.getElementById("o1");
             wRep.classList.add("activeReport");
+
+
         }
 
         function lbkAdd(){
+
         }
         
     </script>
--- sql/canteen.sql
+++ sql/canteen.sql
@@ -5,3 +5,5 @@ CREATE TABLE `canteen_transactions`.`tbl_trans_logs` ( `transaction_id` INT NOT 
 CREATE TABLE `canteen_transactions`.`login` ( `loginID` INT NOT NULL AUTO_INCREMENT , `userName` VARCHAR(100) NOT NULL , `userPass` VARCHAR(100) NOT NULL , PRIMARY KEY (`loginID`)) ENGINE = InnoDB;
 
 INSERT INTO `login`(`loginID`, `userName`, `userPass`) VALUES (null,'canteen','canteen')
+
+CREATE TABLE `canteen_transactions`.`logbooksales` ( `logbook_ID` INT NOT NULL AUTO_INCREMENT , `lgbk_date` DATE NOT NULL , `lgbk_name` VARCHAR(100) NOT NULL , `lgbk_employer` VARCHAR(100) NOT NULL , PRIMARY KEY (`logbook_ID`)) ENGINE = InnoDB;
