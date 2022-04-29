f4c033cacf749ee428c9126d8c7c8402fff55245
John Arian Malondras <arian.malondras@gpi.com.ph>
2022-04-29 15:58:38 +0800

    logbookSales - Modal

--- logbookSales.php
+++ logbookSales.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+
+    include("./connection.php");
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +19,25 @@
 </head>
 
 <body id="logbookBody" onload="navFuntion()">
+
+    <?php
+        if(isset($_POST['lgbkCancel'])){
+            unset($_SESSION['selEmp']);
+            $_SESSION['selEmp'] = "0";
+            header('location: logbookSales.php');
+        }
+
+        if(isset($_POST['lgbkAdd'])){
+            $lgbkDate = $_POST['lgbkInputDate'];
+            $lgbkName = strtoupper($_POST['lgbkInputName']);
+            $lgbkEmp = strtoupper($_POST['lgbkOption']);
+
+            
+
+            header('location: logbookSales.php');
+        }
+
+    ?>
 
     <!-- Include Navigation Side Bar -->
     <?php require_once 'nav.php';?>
@@ -63,23 +89,23 @@
         </div>
     </div>
 
-    <div class="lgbkBG">
+    <div class="lgbkBG" id="lgbkBG" style="visibility:<?php if(!isset($_SESSION['selEmp']) || $_SESSION['selEmp'] == '0'){ echo 'hidden;'; }else{ echo 'visible;'; } ?>;">
         <div class="lgbkModal">
-            <form action="POST" class="lgbkForm">
-                <h1 class="lgbkFormTitle">ADD</h1>
+            <form method="POST" class="lgbkForm">
+                <h1 class="lgbkFormTitle" id="lgbkFormTitle">ADD</h1>
                 <table class="lgbkFormTable">
                     <tr>
                         <td>Date</td>
-                        <td><input type="date" name="" id=""></td>
+                        <td><input type="date" name="lgbkInputDate" data-date="" data-date-format="DD-MM-YYYY" id="lgbkInputDate"></td>
                     </tr>
                     <tr>
                         <td>Full Name</td>
-                        <td><input type="text" name="" id=""></td>
+                        <td><input type="text" name="lgbkInputName"  id="lgbkInputName" autocomplete="off" onkeyup="lgbkUp()"></td>
                     </tr>
                     <tr>
                         <td>Employer</td>
                         <td>
-                            <select id="empOption" name="empOption" change onchange="FkeyDown()">
+                            <select id="lgbkOption" name="lgbkOption" change onchange="lgbkUp()">
                             <option value="" disabled hidden selected>Select your option</option>
                             <option value="glory">GLORY</option>
                             <option value="maxim">MAXIM</option>
@@ -90,8 +116,11 @@
                     </tr>
                 </table>
                 <div class="lgbkModalBtn">
-                    <input type="button" name="lgbkSaveBtn" class="lgbkSaveBtn" id="lgbkSaveBtn" value="Save" onclick="lgbkSaveBtn()">
-                    <input type="button" name="lgbkCloseModal" class="lgbkCloseModal" id="lgbkCloseModal" value="Cancel" onclick="lgbkCloseModal()">
+                    <input type="submit" name="lgbkAdd" id="lgbkAdd" value="Add" disabled>
+                    <input type="submit" name="lgbkEdit" id="lgbkEdit" value="Edit" disabled>
+                    <input type="submit" name="lgbkCancel" id="lgbkCancel" value="Cancel">
+                    <input type="button" name="lgbkSaveBtn" class="lgbkSaveBtn" id="lgbkSaveBtn" value="Save" onclick="lgbkSav3Btn()">
+                    <input type="button" name="lgbkCloseModal" class="lgbkCloseModal" id="lgbkCloseModal" value="Cancel" onclick="lgbkCloseM0dal()">
                 </div>
             </form>
         </div>
@@ -108,8 +137,55 @@
 
         }
 
-        function lbkAdd(){
+        var dateVal = document.getElementById("lgbkInputDate");
+        var nameVal = document.getElementById("lgbkInputName");
+        var empSel = document.getElementById("lgbkOption");
+        var SbmtAdd = document.getElementById("lgbkAdd");
+        var SbmtEdit = document.getElementById("lgbkEdit");
 
+        function lbkAdd(){
+            document.getElementById("lgbkFormTitle").innerHTML = "ADD"
+            dateVal.value = "";
+            nameVal.value = "";
+            empSel.selectedIndex = 0;
+            document.getElementById("lgbkBG").style.visibility = "visible";
+            document.getElementById("lgbkInputName").focus();
+            
+            var date = new Date();
+            var currentDate = date.toISOString().substring(0,10);
+            document.getElementById('lgbkInputDate').value = currentDate;
+        }
+
+        function lgbkUp(){
+            if(nameVal.value === "" || empSel.selectedIndex === 0){
+                SbmtAdd.disabled = true;
+                SbmtEdit.disabled = true;
+            }else{
+                if(document.getElementById("lgbkFormTitle").innerHTML == "ADD"){
+                    SbmtAdd.disabled = false;
+                }else if(document.getElementById("lgbkFormTitle").innerHTML == "EDIT"){
+                    SbmtEdit.disabled = false;
+                }
+            }
+        }
+
+        function lgbkSav3Btn(){
+            if(nameVal.value == ""){
+                swal ( "Oops" ,  "You have entered an invalid Employee Name!" ,  "error" );
+            }else if(empSel.selectedIndex === 0){
+                swal ( "Oops" ,  "You have selected an invalid Employer!" ,  "error" );
+            }else{
+                if(document.getElementById("lgbkFormTitle").innerHTML == "ADD"){
+                    document.getElementById("lgbkAdd").click();
+                }else if(document.getElementById("lgbkFormTitle").innerHTML == "EDIT"){
+                    document.getElementById("lgbkEdit").click();
+                }
+            }
+        }
+
+        function lgbkCloseM0dal(){
+            document.getElementById("lgbkCancel").click();
+            console.log("test");
         }
         
     </script>
