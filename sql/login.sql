f40cd96c9c7cf3d014ace9b9a952f4c0c6a3d6b9
John Arian <malondrasarian@gmail.com>
2022-06-07 17:28:47 +0800

    Delete login.sql

--- sql/login.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `canteen_transactions`.`login` ( `loginID` INT NOT NULL AUTO_INCREMENT , `userName` VARCHAR(100) NOT NULL , `userPass` VARCHAR(100) NOT NULL , PRIMARY KEY (`loginID`)) ENGINE = InnoDB;
-
-INSERT INTO `login`(`loginID`, `userName`, `userPass`) VALUES (null,'canteen','canteen')
